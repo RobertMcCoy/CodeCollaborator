cb98b58f8b2c782f93132c424254abb3fac3412f
lockdown6435 <rmccoy6435@gmail.com>
2017-09-26 18:53:46 -0400

    clean: refactored server code and cleaned up git repo

--- CodeCollaborator.API/server.js
+++ CodeCollaborator.API/server.js
@@ -5,27 +5,19 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var path = require('path');
 
-app.use('/js', express.static(path.join(__dirname, 'js')));
-
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
-
+server.listen(port);
 
 var url = require('url');
 const uuidv4 = require('uuid/v4');
-
 
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
 
-
 io.on('connection', function (socket) {
   io.set('transports', ['websocket']);
 
   socket.on('connectToRoom', function (data) {
-    console.log('Resolving for new connection');
     var roomId = "";
     if (data != null && data.roomId != null) {
       roomId = data.roomId;
