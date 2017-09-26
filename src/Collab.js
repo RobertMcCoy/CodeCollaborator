98fd7b1d86265f1712b9d97df8d6b0f16ba1a7ab
Robert McCoy <rmccoy6435@gmail.com>
2017-09-26 14:43:44 -0400

    feat: readded id to URL bar

--- src/Collab.js
+++ src/Collab.js
@@ -5,17 +5,26 @@ class Collab extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            roomId: "",
-            code: "",
+            roomId: this.props.id || "",
+            code: "Begin writing your code here..."
         };
-        subscribeToRoom((err, roomId) => this.setState({
-            roomId: roomId
-        }));
+
+        subscribeToRoom((err, roomId, message) => {
+                this.setState({
+                    roomId: roomId,
+                    message: message
+                });
+                window.history.pushState({}, 'Collab', '/?id=' + roomId);
+            }
+        );
+
         getCodeUpdate((err, roomId, code) =>
             this.setState({
                 roomId: roomId,
                 code: code
-            }));
+            })
+        );
+
         this.handleChange = this.handleChange.bind(this);
     }
 
@@ -23,9 +32,13 @@ class Collab extends Component {
         submitCodeUpdate(this.props.roomId, event.target.value);
     };
 
+    componentDidMount() {
+        console.log(this.state.roomId);
+    }
+
     render() {
         return (
-            <textarea name="code" id="codeSpace" cols="30" rows="10" onChange={this.handleChange}>{this.props.code}</textarea>
+            <textarea name="code" id="codeSpace" value={this.state.code} cols="30" rows="10" onChange={this.handleChange} />
         );
     }
 }
