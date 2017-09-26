9fdde95e3d380940329bb868aae5b332d7bb36d0
lockdown6435 <rmccoy6435@gmail.com>
2017-09-25 20:54:23 -0400

    feat: Added initial socket connection

---
+++ app.js
@@ -0,0 +1,31 @@
+var url = require('url');
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+const uuidv4 = require('uuid/v4');
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+
+  socket.on('connectToRoom', function (data) {
+    console.log('Resolving for new connection');
+    let roomId = "";
+    if (data != null && data.roomId != null) {
+      roomId = data.roomId;
+    }
+    if (roomId == "") {
+      roomId = uuidv4();
+    }
+    socket.join(roomId);
+    io.sockets.in(roomId).emit('newConnection', { message: 'Connected to new room: ' + roomId, roomId: roomId });
+  });
+
+  socket.on('codeChange', function (data) {
+    console.log('Update to input detected: ' + data.code);
+    io.sockets.in(data.roomId).emit('codeUpdate', data)
+  });
+});
