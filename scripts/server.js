cfe40f47d0420b6ed4c892093fe9a8e1199cb836
Robert McCoy <rmccoy@apterainc.com>
2017-10-02 08:52:54 -0400

    try: attempted fix at the routing issues when sharing a link (I think express was trying to take control of the routing rather than React)

--- scripts/server.js
+++ scripts/server.js
@@ -12,7 +12,7 @@ const uuidv4 = require('uuid/v4');
 
 app.use(express.static(path.join(__dirname, "/../build")));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + "/../build/index.html"));
 });
 
