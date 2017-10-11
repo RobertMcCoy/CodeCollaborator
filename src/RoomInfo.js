919789eaf905f7501a49f5a04b70a8836f0e4aed
David Meredith <dmeredith.1996@gmail.com>
2017-10-11 08:42:46 -0400

    wip: added a tab feature on the bottom right. May or may not keep.

--- src/RoomInfo.js
+++ src/RoomInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Users from './Users';
 import './RoomInfo.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class RoomInfo extends Component {
 
@@ -22,6 +23,19 @@ class RoomInfo extends Component {
             <div className="room-info">   
                 <Users collaborators={this.state.collaborators}/>
                 <hr/>
+                <Tabs>
+                    <TabList>
+                        <Tab>Tab 1</Tab>
+                        <Tab>Tab 2</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <h2>Ex. Tab 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Ex. Tab 2</h2>
+                    </TabPanel>               
+                </Tabs>
             </div>
         );
     }
