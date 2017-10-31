39c649d0633c92d989e2f3ee81dc0921e972ca36
David Meredith <dmeredith.1996@gmail.com>
2017-10-31 12:35:24 -0400

    wip: Landing page banner added and more content added.

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -13,12 +13,20 @@ class LandingPage extends Component {
     render(){
         return(
             <div className="landing-page">
-                <h1>Code Collaborator</h1>
-                <h3>Code together, anywhere, anytime.</h3>
+                <img src="codeCollabBanner.svg" className="banner"/>
                 <h1 className="section-head">About the App</h1>
-                <p className="section-info">Insert cool stuff about the app here.</p>
-                <h1 className="section-head">About the Developers</h1>
-                <p className="section-info">Insert cool stuff about the devs here.</p>
+                <p className="section-info">Have you ever wanted to collaboratively code with your peers? Now you can. CodeCollaborator is a React application that utilizes Socket.IO to allow people to collaborate on code from any machine.</p>
+                <h1 className="section-head">The Devs</h1>
+                <div className="dev-section">
+                    <h3>Robert</h3>
+                </div>
+                <div className="dev-section">
+                    <h3>David</h3>
+                </div>
+                <div className="dev-section">
+                    <h3>Brad</h3>
+                </div>
+                
             </div>
         );
     }
