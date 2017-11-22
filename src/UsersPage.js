030deb1d8808b9edb87c498eec5bd4c59e171941
David Meredith <dmeredith.1996@gmail.com>
2017-11-21 20:08:31 -0500

    generic user page and reset password section added.

--- src/UsersPage.js
+++ src/UsersPage.js
@@ -5,11 +5,45 @@ class UsersPage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            hidden: false,
+            hidden: true,
             oldPass: '',
             newPass: '',
             newPassRepeated: ''
         }
+        this.handleResetPasswordClick = this.handleResetPasswordClick.bind(this);
+        this.handleOldPassField = this.handleOldPassField.bind(this);
+        this.handleNewPassField = this.handleNewPassField.bind(this);
+        this.handleNewPassRepeatedField = this.handleNewPassRepeatedField.bind(this);
+    }
+
+    handleResetPasswordClick () {
+        if(this.state.hidden == false) {
+            this.setState({
+                hidden: true
+            });
+        } else {
+            this.setState({
+                hidden: false
+            });
+        }
+    }
+
+    handleOldPassField(event) {
+        this.setState({
+            oldPass: event.target.value
+        });
+    }
+
+    handleNewPassField(event) {
+        this.setState({
+            newPass: event.target.value
+        });
+    }
+
+    handleNewPassRepeatedField(event) {
+        this.setState({
+            newPassRepeated: event.target.value
+        });
     }
 
     render () {
@@ -21,13 +55,25 @@ class UsersPage extends Component {
                 <br/>
                 <h3 className="last-login">lastLogin</h3>
                 <br/>
-                <button className="reset-pass">Reset Password</button>
-
+                <button className="reset-pass" onClick={this.handleResetPasswordClick}>Reset Password</button>
+                <br/>
+                <br/>
                 <div className={this.state.hidden ? 'hidden' : 'change-pass'}>
-                    <h3>Old Password: <input value={this.state.oldPass}/></h3> 
-                    <h3>New Password: <input value={this.state.newPass}/></h3>
-                    <h3>Retype New Password: <input value={this.state.newPassRepeated}/></h3>
-                    <button className='submit-button'>Submit</button>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Old Password: <input type="password" value={this.state.oldPass} onChange={this.handleOldPassField}/>
+                        </label>
+                        <br/> 
+                        <label>
+                            New Password: <input type="password" value={this.state.newPass} onChange={this.handleNewPassField}/>
+                        </label> 
+                        <br/>
+                        <label>
+                            Retype New Password: <input type="password" value={this.state.newPassRepeated} onChange={this.handleNewPassRepeatedField}/>
+                        </label> 
+                        <br/>
+                        <input type="submit" value="Submit" />
+                    </form>
                 </div>
             </div>
         );
