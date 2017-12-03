b53b671cb27d42fa9db8ef7a0a4192a0110bf00d
Robert McCoy <robert@robertmccoy.org>
2017-12-03 03:51:44 -0500

    clean: cleaned up HTML code across the site for responsive design; update logo and made site more ADA compliant; landing page is now a pure component; added in dev info

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './LandingPage.css';
 
-class LandingPage extends Component {
-    constructor(props){
-        super(props);
-
-        this.state={
-
-        }
-    }
-
-    render(){
-        return(
+const LandingPage = (props) => {
+        return (
             <div className="landing-page">
-                <img src="codeCollabBanner.svg" className="banner" alt="CodeCollab Logo"/>
-                <h1 className="section-head">About the App</h1>
-                <p className="section-info">Have you ever wanted to collaboratively code with your peers? Now you can. CodeCollaborator is a React application that utilizes Socket.IO to allow people to collaborate on code from any machine.</p>
-                <h1 className="section-head">The Devs</h1>
-                <div className="dev-section">
-                    <h3>Robert</h3>
+                <img src={logo} className="banner" alt="CodeCollab Logo" />
+                <h2 className="section-head">About the App</h2>
+                <p className="section-info">Have you ever wanted to collaboratively code with your peers? Now you can. CodeCollaborator is a React application that utilizes Socket.IO to allow people to collaborate on code from any device.</p>
+                <h2 className="section-head">The Devs</h2>
+                <div class="row">
+                    <div className="dev-section col-md-4 col-xs-12">
+                        <h3>Robert</h3>
+                        <img src="assets/images/rmccoy.jpg" alt="Robert Profile"/>
+                        <p>Senior at IPFW and Web Developer at Aptera, Inc. Enjoys gifs of puppies far more than he probably should.</p>
+                    </div>
+                    <div className="dev-section col-md-4 col-xs-12">
+                        <h3>David</h3>
+                        <img src="assets/images/dm_profilepicture_close.jpg" alt="David Profile"/>
+                        <p>Senior at IPFW, enjoys software development and long walks on the beach.</p>
+                    </div>
+                    <div className="dev-section col-md-4 col-xs-12">
+                        <h3>Brad</h3>
+                        <p>A senior at IPFW and a Software Engineer for IDEMIA.</p>
+                    </div>
                 </div>
-                <div className="dev-section">
-                    <h3>David</h3>
-                    <img src="dm_profilepicture_close.jpg"/>
-                    <p>Senior at IPFW, enjoys software development and long walks on the beach.</p>
-                </div>
-                <div className="dev-section">
-                    <h3>Brad</h3>
-                    <p>A senior at IPFW and a Software Engineer for IDEMIA.</p>
-                    
-                </div>
-                
+
             </div>
         );
-    }
 }
 
 export default LandingPage;
